f822440eb049c9d032f79393618a7ecb3c6feb28
UstinovaTatiana <ustinta@gmail.com>
2025-08-16 10:26:14 +0300

    переустановлен prettier

--- modules/comments.js
+++ modules/comments.js
@@ -1,21 +1,21 @@
 export const commentsData = [
-    {
-      id: 1,
-      name: "Глеб Фокин",
-      date: "12.02.22 12:18",
-      text: "Это будет первый комментарий на этой странице",
-      likesCount: 3,
-      liked: false,
-      replies: []
-    },
-    {
-      id: 2,
-      name: "Варвара Н.",
-      date: "13.02.22 19:22",
-      text: "Мне нравится как оформлена эта страница! ❤",
-      likesCount: 75,
-      liked: true,
-      replies: []
-    }
-  ];
-  
+  {
+    id: 1,
+    name: "Глеб Фокин",
+    date: "12.02.22 12:18",
+    text: "Это будет первый комментарий на этой странице",
+    likesCount: 3,
+    liked: false,
+    replies: [],
+  },
+
+  {
+    id: 2,
+    name: "Варвара Н.",
+    date: "13.02.22 19:22",
+    text: "Мне нравится как оформлена эта страница! ❤",
+    likesCount: 75,
+    liked: true,
+    replies: [],
+  },
+];
